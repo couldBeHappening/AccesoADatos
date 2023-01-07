feeea8aff6f30a7711e02f201e3948342972c5f6
couldBeHappening <raqmc.89@gmail.com>
2023-01-07 20:10:18 +0100

    cositas practica3

--- AccesoADatos_Practica3_ManejoDeConectores(III)/src/practica_ManejoDeConectores/Enunciado.java
+++ AccesoADatos_Practica3_ManejoDeConectores(III)/src/practica_ManejoDeConectores/Enunciado.java
@@ -1,9 +1,50 @@
 package practica_ManejoDeConectores;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.text.DecimalFormat;
+
 public class Enunciado {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		try {
+			
+			Connection conexion = DriverManager.getConnection("jdbc:mysql://localhost/empleados","vespertino","password");
+			Statement sentencia = conexion.createStatement();
+			String consulta = "SELECT * FROM empleado";
+			ResultSet resultado = sentencia.executeQuery(consulta);
+			ResultSetMetaData resultadoMeta = resultado.getMetaData();
+			
+			String departamento = args [0];
+			int departamento = Integer.parseInt(departamento);
+			DecimalFormat formato = new DecimalFormat("##,##0.00");
+			
+			PreparedStatement sentenci = conexión.prepareStatement(sql);
+
+
+			String primerApellido = args[1];
+			String nif = args[2];
+			String salarioEmpleados = args[3];
+			
+			if (resultado.next() == false) {
+				
+				System.out.println("El departamento " + depatamento + " no existe en la BBDD ");
+			
+			} else {
+				
+			}
+			
+		} catch (SQLException e){
+			
+		}
+			
+		
 
 	}
 
