3ea2b95fe9d3562fb696f724a0ae8369c5f102d0
moyoman <moi_roque@hotmail.com>
2023-01-11 11:36:44 +0100

    primera correción de comentarios en código

--- AccesoADatos_Practica3_ManejoDeConectores(III)/src/practica_ManejoDeConectores/Enunciado.java
+++ AccesoADatos_Practica3_ManejoDeConectores(III)/src/practica_ManejoDeConectores/Enunciado.java
@@ -17,29 +17,34 @@ public class Enunciado {
 				/* Inicializamos la conexión con la BBDD */
 				Connection conexion = DriverManager.getConnection("jdbc:mysql://localhost/empleados","vespertino","password");
 
-				/* Inicializamos las variable que vamos a utilizar */
+				/* Inicializamos las variables que vamos a utilizar */
 				String departamento = args [0];
 				int departamentoParseo = Integer.parseInt(departamento);
 				DecimalFormat formatoDecimal = new DecimalFormat("##,##0.00");
 
-				/* Preparación de la primera consulta que vamos a realizar */
+				/* Preparación de la primera consulta que vamos a realizar, se utilizan los marcadores (?) para indicar dónde
+				 * van los datos añadidos después */
 				String sql = "SELECT apellido1,nif,salario FROM empleado WHERE codigo_departamento = (?) ";
 				PreparedStatement sentencia = conexion.prepareStatement(sql);
 
+				/* Añadimos la variable a la consulta preparada y la ejecutamos en la BBDD */
 				sentencia.setInt(1, departamentoParseo);
 				ResultSet resultado = sentencia.executeQuery();
 
-				/* Preparación de la segunda consulta que vamos a realizar */
+				/* Preparación de la segunda consulta que vamos a realizar , se utilizan los marcadores (?) para indicar dónde
+				 * van los datos añadidos después */
 				String sql2 = "SELECT AVG(salario),dep.nombre FROM empleado emple,departamento dep WHERE dep.codigo = (?) AND emple.codigo_departamento=dep.codigo"; 
 				PreparedStatement sentencia2 = conexion.prepareStatement(sql2);
 
+				/* Añadimos la variable a la consulta preparada y la ejecutamos en la BBDD */
 				sentencia2.setInt(1, departamentoParseo);
 				ResultSet resultado2 = sentencia2.executeQuery();
 
-				/* Condicional if - else para comprobar */
+				/* Condicional if - else para comprobar si la consulta nos devuelde resultados */
 				
 				if (resultado.next() == true) {
 
+					/* Si hay resultados recuperamos la media y el nombre y lo imprimimos */
 					resultado2.next();
 
 					Float media = resultado2.getFloat(1);
@@ -48,7 +53,7 @@ public class Enunciado {
 
 					System.out.println("Los empleados del departamento " + nombreDepartamento + ", con código " + departamento + " son:");
 
-					/* Bucle do - while para que haga */
+					/* Bucle do - while que recorre todos los resultados de la primera consulta y los muestra */
 					do { 
 
 						String apellido1 = resultado.getString(1);
@@ -57,8 +62,9 @@ public class Enunciado {
 
 						System.out.println("El primer apellido del empleado es: " + apellido1 + " con NIF: " + nif + ", y tiene un salario de: " + salario + "€");
 
-					} while (resultado.next()); /* Mientras */
+					} while (resultado.next());
 
+					/* Mostramos la media con formato */
 					String salarioMedio = formatoDecimal.format(media);
 					System.out.println("El salario medio del departamento " + nombreDepartamento + " es: " + salarioMedio + "€");
 
@@ -68,18 +74,18 @@ public class Enunciado {
 
 				} 
 				
-				/* Cerramos todos los objetos */
+				/* Cerramos todos los objetos SQL*/
 				conexion.close();
 				sentencia.close();
 				sentencia2.close();
 				resultado.close();
 				resultado2.close();
 
-				/* Capturamos la SQLException para evitar los errores que nos surjan con las querys */
+				/* Capturamos las SQLException para manejar los errores que nos surjan con las querys */
 			} catch (SQLException e){
 				System.out.println("¡Error! no se han encontardo los datos consultados");
 
-				/* Capturamos la NumberFormatException para evitar los errores que nos surjan si no se introduce un argumento diferente a un número */
+				/* Capturamos la NumberFormatException para evitar los errores que nos surjan si se introduce un argumento diferente a un número */
 			} catch (NumberFormatException e) {
 				System.out.println("El valor introducido en los arguemntos no es correcto. ¡Vuelva a intentarlo!");
 			}
